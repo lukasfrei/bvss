89fd115bc3823ae7b4a44a7a3a55864e4fdf41e2
Lukas Frei <lukas.frei@frei-web.ch>
2021-10-11 20:40:59 +0200

    added logo footer

--- footer.php
+++ footer.php
@@ -15,6 +15,88 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
+<div class="container">
+<hr>
+            <div class="row mitglied">
+                <div class="col-12 col-md-6">
+                    <p class="text-left">Eine Fachkommission von:</p>
+                    <a href="https://www.bildungbern.ch" target="_blank"><img
+                                src="/user/themes/bvss/images/logo_berufsverband_bildung_bern.svg" id="bildungbern"
+                                class="img-responsive"></a>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p class="text-left">Mitglied im:</p>
+                    <a href="https://www.svss.ch/" target="_blank"><img src="/user/themes/bvss/images/svss.png"
+                                                                        id="svss" class="img-responsive"></a>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <br>
+                <div class="col-md-12">
+                    <p>Unsere Partner:</p>
+                    <br>
+                </div>
+            </div>
+
+
+            <div class="row logos1">
+                <div class="col-6 col-md-3">
+                    <a href="https://campusperspektiven.ch/" target="_blank">
+                        <img src="/user/themes/bvss/images/perspektiven.jpg" id="perspektiven"
+                             class="partner img-responsive">
+                    </a>
+                </div>
+                <div class="col-6 col-md-3">
+                <a href="https://www.vistawell.ch/de_DE/" target="_blank">
+                    <img src="/user/themes/bvss/images/vistavell.png" id="vistawell" class="partner img-responsive">
+                </a>
+                </div>
+                <div class="col-6 col-md-3">
+                <a href="https://schumacher-sport.ch/content/news/index_ger.html" target="_blank">
+                    <img src="/user/themes/bvss/images/schumacher.jpg" id="schumacher"
+                         class="partner img-responsive">
+                </a>
+                </div>
+                <div class="col-6 col-md-3">
+                <a href="https://www.radys.swiss/" target="_blank">
+                    <img src="/user/themes/bvss/images/radys.png" id="radys" class="partner img-responsive">
+                </a>
+                </div>
+
+            </div>
+
+            <div class="row logos2">
+                <div class="col-6 col-md-3">
+                <a href="https://friedrich-sport.ch/" target="_blank">
+                    <img src="/user/themes/bvss/images/friedrich.jpeg" id="friedrich" class="partner img-responsive">
+                 </a>
+                </div>
+                <div class="col-6 col-md-3">
+                <a href="https://www.tls.ch/" target="_blank">
+                    <img src="/user/themes/bvss/images/thometluescher.png" id="thometluescher"
+                         class="partner img-responsive">
+                 </a>
+                </div>
+                <div class="col-6 col-md-3">
+                <a href="https://thoemus.ch/" target="_blank">
+                    <img src="/user/themes/bvss/images/thoemus.png" id="thoemus" class="partner img-responsive">
+                 </a>
+                </div>
+                <div class="col-6 col-md-3">
+                <a href="https://www.ski-velo-center.ch/" target="_blank">
+                    <img src="/wp-content/themes/bvss-child-theme/img/skivelo.jpg" id="skivelo" class="partner img-responsive">
+                 </a>
+                </div>
+            </div>
+                        
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+
 <div class="wrapper" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
